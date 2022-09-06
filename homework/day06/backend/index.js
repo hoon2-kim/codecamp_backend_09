f134dde0a0847980760b6301ce013f0387e83f88
hoon2-kim <gooaba1204@gmail.com>
2022-09-06 19:57:13 +0900

    7일차 수업

--- homework/day06/backend/index.js
+++ homework/day06/backend/index.js
@@ -17,6 +17,94 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerJsdoc(options)));
 app.use(express.json());
 app.use(cors());
 
+app.get("/users", (req, res) => {
+  const usersData = [
+    {
+      email: "aaa@aaa.com",
+      name: "철수",
+      phone: "010-1234-5678",
+      personal: "220110-2222222",
+      prefer: "https://naver.com",
+    },
+    {
+      email: "Nick@nick.com",
+      name: "Nick",
+      phone: "010-1234-5678",
+      personal: "220219-0000000",
+      prefer: "https://naver.com",
+    },
+    {
+      email: "Judy@judy.com",
+      name: "Judy",
+      phone: "010-1234-5678",
+      personal: "220219-0000000",
+      prefer: "https://naver.com",
+    },
+    {
+      email: "Anna@anna.com",
+      name: "Anna",
+      phone: "010-1234-5678",
+      personal: "220219-0000000",
+      prefer: "https://naver.com",
+    },
+    {
+      email: "Elsa@elsa.com",
+      name: "Elsa",
+      phone: "010-1234-5678",
+      personal: "220219-0000000",
+      prefer: "https://naver.com",
+    },
+  ];
+  res.send(usersData);
+});
+
+// 과제 2
+app.get("/starbucks", (req, res) => {
+  const coffeeLists = [
+    {
+      name: "아메리카노",
+      kcal: 5,
+    },
+    {
+      name: "카페라떼",
+      kcal: 10,
+    },
+    {
+      name: "콜드브루",
+      kcal: 15,
+    },
+    {
+      name: "카페모카",
+      kcal: 50,
+    },
+    {
+      name: "돌체라떼",
+      kcal: 500,
+    },
+    {
+      name: "카라멜라떼",
+      kcal: 200,
+    },
+    {
+      name: "바닐라라떼",
+      kcal: 20,
+    },
+    {
+      name: "에스프레소",
+      kcal: 1,
+    },
+    {
+      name: "디카페인",
+      kcal: 5,
+    },
+    {
+      name: "오트라떼",
+      kcal: 300,
+    },
+  ];
+  res.send(coffeeLists);
+});
+
 // 과제 1
 app.post("/tokens/phone", (req, res) => {
   console.log(req.body);
--- homework/day06/frontend/login/signup.js
+++ homework/day06/frontend/login/signup.js
@@ -22,26 +22,26 @@ const getValidationNumber = async () => {
 const submitSignup = async () => {
   console.log("회원 가입 이메일 전송");
 
-  const name = document.getElementById("SignupName").value;
-  const registerNum = document.getElementById("SignupPersonal").value;
-  const site = document.getElementById("SignupPrefer").value;
-  const email = document.getElementById("SignupEmail").value;
-  const pass = document.getElementById("SignupPwd").value;
+  // const name = document.getElementById("SignupName").value;
+  // const registerNum = document.getElementById("SignupPersonal").value;
+  // const site = document.getElementById("SignupPrefer").value;
+  // const email = document.getElementById("SignupEmail").value;
+  // const pass = document.getElementById("SignupPwd").value;
   const phone1 = document.getElementById("PhoneNumber01").value;
   const phone2 = document.getElementById("PhoneNumber02").value;
   const phone3 = document.getElementById("PhoneNumber03").value;
   const phoneNumber = phone1 + phone2 + phone3;
 
-  await axios
-    .post("http://localhost:3000/users", {
-      name,
-      registerNum,
-      site,
-      email,
-      pass,
-      phoneNumber,
-    })
-    .then((res) => {
-      console.log(res);
-    });
+  const signUp = {
+    name: document.getElementById("SignupName").value,
+    registerNum: document.getElementById("SignupPersonal").value,
+    site: document.getElementById("SignupPrefer").value,
+    email: document.getElementById("SignupEmail").value,
+    pass: document.getElementById("SignupPwd").value,
+    phoneNumber,
+  };
+
+  await axios.post("http://localhost:3000/users", signUp).then((res) => {
+    console.log(res);
+  });
 };
